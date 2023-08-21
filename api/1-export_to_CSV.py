39750de042be7abae8f21a3b6622922eb91fd2e5
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-08-21 02:55:21 +0200

    add task 1, tested + styled

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""program that uses a fake api to retreive data."""
+
+import csv
+import requests
+import sys
+
+if len(sys.argv) > 1:
+    user_id = sys.argv[1]
+
+    url_name = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    url_todos = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    name_request = requests.get(url_name)
+    todos_request = requests.get(url_todos)
+
+    if name_request.status_code == 200:
+        name_response = name_request.json()
+        name = name_response["name"]
+
+    if todos_request.status_code == 200:
+        todos_response = todos_request.json()
+        completed = []
+        uncompleted = []
+        for task in todos_response:
+            if task['completed']:
+                completed.append(task)
+            else:
+                uncompleted.append(task)
+        todos_total = (len(completed) + len(uncompleted))
+
+    output = "Employee {} is done with tasks({}/{}):".format(
+        name, len(completed), todos_total)
+    print(output)
+    for task_name in completed:
+        print(f"\t {task_name['title']}")
+
+    csv_list = []
+    for task in todos_response:
+        csv_list.append([user_id, name, task["completed"], task["title"]])
+    file_name = "USER_ID.csv"
+
+    with open(file_name, mode="w", newline='') as file:
+        writer = csv.writer(file)
+
+        writer.writerows(csv_list)
+
+else:
+    print("usage: command userid")
