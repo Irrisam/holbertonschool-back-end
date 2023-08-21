03f466b0acf8baf3ccd3bf766b1b2feaf8029ebc
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-08-21 02:01:33 +0200

    add first task tested+ styled

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+import sys
+import requests
+
+if len(sys.argv) > 1:
+    user_id = sys.argv[1]
+
+    url_name = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    url_todos = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    name_request = requests.get(url_name)
+    todos_request = requests.get(url_todos)
+
+    if name_request.status_code == 200:
+        name_response = name_request.json()
+        name = name_response["name"]
+
+    if todos_request.status_code == 200:
+        todos_response = todos_request.json()
+        completed = []
+        uncompleted = []
+        for task in todos_response:
+            if task['completed']:
+                completed.append(task)
+            else:
+                uncompleted.append(task)
+        todos_total = (len(completed) + len(uncompleted))
+
+    print(f"Employee {name} is done with tasks{len(completed)}/{todos_total}:")
+    for task_name in completed:
+        print(f"\t {task_name['title']}")
+else:
+    print("Please provide a user_id as a command-line argument.")
